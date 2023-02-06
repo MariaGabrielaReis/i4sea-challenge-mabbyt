15e1c9a7c4627b4f74d92c7c2c682d55db24f4ff
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-02-06 06:18:30 -0300

    chore: install libs and configurate project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/theme";
+
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) return <Loading />;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
+      <Home />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF",
+    BLACK: "#09090A",
+    GRAY: "#777777",
+    TEAL: "#0D9488",
+  },
+  FONT_FAMILY: {
+    REGULAR: "Inter_400Regular",
+    BOLD: "Inter_700Bold",
+  },
+  FONT_SIZE: {
+    XS: 10,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+  },
+};
