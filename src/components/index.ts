1fa55579c46e1d71c7ac538afa1d4c9a622ebf24
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-02-06 07:43:41 -0300

    feat: create components index

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { Header } from "./Header";
+import { Loading } from "./Loading";
+import { Navbar } from "./Navbar";
+import { Post } from "./Post";
+
+export { Header, Loading, Navbar, Post };
