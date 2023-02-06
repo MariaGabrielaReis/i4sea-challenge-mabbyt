20c1e4dfa5f12989a046bf3d087081c50fd809f2
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-02-06 07:43:54 -0300

    feat: use components at home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,33 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView, FlatList } from "react-native";
+
+import { Header, Navbar, Post } from "../../components";
+import { useCategoryContext } from "../../context/CategoryContext";
 
 export function Home() {
+  const { posts } = useCategoryContext();
+
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView>
+      <Header />
+      <Navbar />
+
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => (
+          <Post
+            id={item.id}
+            title={item.title}
+            author={item.author}
+            votes={item.votes}
+            comments={item.comments}
+            postedAt={item.postedAt}
+            url={item.url}
+            imageSource={item.imageSource}
+          />
+        )}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
